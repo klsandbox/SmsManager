ddefbb527f2eb2fc875de611ef91fffc00f2e922
Ibrahim Abdul Rahim <mrwaim@gmail.com>
2016-03-30 12:29:25 +0800

    route middleware

--- routes/routes.php
+++ routes/routes.php
@@ -2,7 +2,7 @@
 
 Route::get('/sms-management/fake-sms', '\Klsandbox\SmsManager\SmsManagementController@getFakeSms');
 
-Route::group(['middleware' => ['auth.admin']], function () {
+Route::group(['middleware' => ['role:admin']], function () {
     Route::get('/sms-management/view', '\Klsandbox\SmsManager\SmsManagementController@getView');
 });
 
